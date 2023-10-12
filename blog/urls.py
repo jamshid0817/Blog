9a47425e15c046e0d9d072267ae65be70f88afe1
user <jamshidotaboyev@example.com>
2023-10-12 20:20:11 +0500

    salom

--- blog/urls.py
+++ blog/urls.py
@@ -27,3 +27,6 @@ from django.conf.urls.static import static
 
 urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
